368739d4823d0f2624bccfd18f7ea34f8556df32
Warren Hansen <whansen1@mac.com>
2018-02-26 18:18:36 -0800

    task: added class csv helper

---
+++ Portfilio Analize/CSVhelp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Portfilio_Analize
+{
+    class CSVhelp
+    {
+        public static string systemPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+		/// 									Get file names
+		/// ////////////////////////////////////////////////////////////////////////////////////////////////
+		public  string[] GetFileNames()
+        {
+
+            var dateTime = DateTime.Today.ToString("MM_dd_yyyy");
+            var filePath = systemPath + @"\Channel" + "_" + dateTime;
+            // Put all file names in root directory into array.
+            string[] array1 = Directory.GetFiles(filePath);
+            //fileCount = array1.Length;
+            return array1;
+        }
+        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+		/// 									Read files in folder
+		/// ////////////////////////////////////////////////////////////////////////////////////////////////
+		public void readAllofThe(string[] fileNames, bool debug)
+        {
+            Console.WriteLine("--- Got " + fileNames.Length.ToString() + " CSV Files: ---");
+            foreach (string name in fileNames)
+            {
+                if (debug) { Console.WriteLine(name); }
+                readAllLinesFrom(filePath: name, debug: debug);
+            }
+        }
+
+        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+        /// 									Read entire CSV 
+        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+        public  void readAllLinesFrom(string filePath, bool debug)
+        {
+
+            try
+            {
+                string[] oneFile = System.IO.File.ReadAllLines(filePath);
+
+                //createStructFrom(oneFile: oneFile, debug: false);
+                Console.WriteLine(oneFile);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nALERT !!! readAllLinesFrom() Exception: " + e.Message);
+            }
+            finally
+            {
+                if (debug) { Console.WriteLine("Executing finally block."); }
+
+            }
+        }
+    }
+}
--- Portfilio Analize/Program.cs
+++ Portfilio Analize/Program.cs
@@ -9,10 +9,46 @@ namespace Portfilio_Analize
 {
     class Program
     {
+        
+        //private static string[] fileNames;
+
+        private struct TradeResults
+        {
+            /// Trade Count, Date In, Date Out, Ticker, profit, cumProfit,
+            public int tradeNum { get; set; }
+            public string dateEntry { get; set; }
+            public string dateExit { get; set; }
+            public string ticker { get; set; }
+            public double profit { get; set; }
+            public double cumProfit { get; set; }
+
+            /// exit name, profit factor, winPct, Consecutive losers, 
+            public string exitName { get; set; }
+            public double profitFactor { get; set; }
+            public double winPct { get; set; }
+            public int consecutiveLosers { get; set; }
+
+            /// largest loser, largest winner, profit per month
+            public double largestLoser { get; set; }
+            public double largestWinner { get; set; }
+            public double profitPerMonth { get; set; }
+        };
+
+        private List<TradeResults> allTradeResults = new List<TradeResults>();
+        private static int fileCount;
+        private int filesParsedCount;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            CSVhelp csvhelp = new CSVhelp();
+            var fileNames = csvhelp.GetFileNames();
+            csvhelp.readAllofThe(fileNames: fileNames, debug: true);
+     
             Console.ReadLine();
         }
+
+        
+        
+       
     }
 }
