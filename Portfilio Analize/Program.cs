c216d4bd9c9f2d6cafdb40abb526a66dfaa4ef5e
Warren Hansen <whansen1@mac.com>
2018-02-27 08:17:09 -0800

    task: added trade result class

--- Portfilio Analize/Program.cs
+++ Portfilio Analize/Program.cs
@@ -10,29 +10,29 @@ namespace Portfilio_Analize
     class Program
     {
     
-        private struct TradeResults
-        {
-            /// Trade Count, Date In, Date Out, Ticker, profit, cumProfit,
-            public int tradeNum { get; set; }
-            public string dateEntry { get; set; }
-            public string dateExit { get; set; }
-            public string ticker { get; set; }
-            public double profit { get; set; }
-            public double cumProfit { get; set; }
+        //private struct TradeResults
+        //{
+        //    /// Trade Count, Date In, Date Out, Ticker, profit, cumProfit,
+        //    public int tradeNum { get; set; }
+        //    public string dateEntry { get; set; }
+        //    public string dateExit { get; set; }
+        //    public string ticker { get; set; }
+        //    public double profit { get; set; }
+        //    public double cumProfit { get; set; }
 
-            /// exit name, profit factor, winPct, Consecutive losers, 
-            public string exitName { get; set; }
-            public double profitFactor { get; set; }
-            public double winPct { get; set; }
-            public int consecutiveLosers { get; set; }
+        //    /// exit name, profit factor, winPct, Consecutive losers, 
+        //    public string exitName { get; set; }
+        //    public double profitFactor { get; set; }
+        //    public double winPct { get; set; }
+        //    public int consecutiveLosers { get; set; }
 
-            /// largest loser, largest winner, profit per month
-            public double largestLoser { get; set; }
-            public double largestWinner { get; set; }
-            public double profitPerMonth { get; set; }
-        };
+        //    /// largest loser, largest winner, profit per month
+        //    public double largestLoser { get; set; }
+        //    public double largestWinner { get; set; }
+        //    public double profitPerMonth { get; set; }
+        //};
 
-        private List<TradeResults> allTradeResults = new List<TradeResults>();
+        //private List<TradeResults> allTradeResults = new List<TradeResults>();
 
         static void Main(string[] args)
         {
